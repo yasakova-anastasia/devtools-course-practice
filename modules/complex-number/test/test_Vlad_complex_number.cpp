f36750cbf5b8650d4747979af2b262cdd618a346
vlad <vlad.devlikamov@gmail.com>
2020-03-22 19:29:34 +0300

    Updated tests

--- modules/complex-number/test/test_Vlad_complex_number.cpp
+++ modules/complex-number/test/test_Vlad_complex_number.cpp
@@ -4,15 +4,20 @@
 
 #include "include/complex_number.h"
 
-TEST(Korniakov_Kirill_ComplexNumberTest, Can_Create_Zero) {
+TEST(Devlikamov_Vlad_ComplexNumberTest, Distributivity_complex_number) {
     // Arrange
-    double re = 0.0;
-    double im = 0.0;
+    double re = 2.0;
+    double im = 4.0;
+    double re_1 = 1.0;
+    double im_1 = 2.0;
+    double re_2 = 3.0;
+    double im_2 = 4.0;
 
     // Act
     ComplexNumber z(re, im);
+    ComplexNumber z1(re_1, im_1);
+    ComplexNumber z2(re_1, im_1);
 
     // Assert
-    EXPECT_EQ(re, z.getRe());
-    EXPECT_EQ(im, z.getIm());
+    EXPECT_EQ((z + z1)*z2, z*z2 + z1*z2);
 }
