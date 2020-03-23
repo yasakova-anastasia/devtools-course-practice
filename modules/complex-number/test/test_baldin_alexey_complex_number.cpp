8ad9e287f47db93254457b78ab9878564c0959ae
Alexey <kret906alex@gmail.com>
2020-03-23 22:35:06 +0300

    Add negative_less_then_positive and math_operations tests

--- modules/complex-number/test/test_baldin_alexey_complex_number.cpp
+++ modules/complex-number/test/test_baldin_alexey_complex_number.cpp
@@ -31,3 +31,46 @@ TEST(Baldin_Alexey_ComplexNumberTest, Multiple_By_Zero) {
 	EXPECT_EQ(z.getRe(), 0.0);
 	EXPECT_EQ(z.getIm(), 0.0);
 }
+
+TEST(Baldin_Alexey_ComplexNumberTest, Negative_Less_Then_Positive) {
+	// Arrange
+	double re1 = 123.0;
+	double re2 = -123.0;
+	double im1 = 456.0;
+	double im2 = -456.0;
+
+	// Act
+	ComplexNumber pos(re1, im1);
+	ComplexNumber neg(re2, im2);
+
+	// Assert
+	EXPECT_LT(neg.getRe(), pos.getRe());
+	EXPECT_LT(neg.getIm(), pos.getIm());
+}
+
+TEST(Baldin_Alexey_ComplexNumberTest, Check_Correct_Math_Operations) {
+	// Arrange
+	double num1 = 10.0;
+	double num2 = 5.0;
+	double num3 = 3.0;
+	double num4 = 2.0;
+
+	double check = 0;
+
+	// Act
+
+	check = num1 + num2 / num3 * num4;
+
+	ComplexNumber cNum1(num1, num1);
+	ComplexNumber cNum2(num2, num2);
+	ComplexNumber cNum3(num3, num3);
+	ComplexNumber cNum4(num4, num4);
+	ComplexNumber cCheck(0.0, 0.0);
+
+	cCheck = cNum1 + cNum2 / cNum3 * cNum4;
+	std::cout << cCheck.getRe() << std::endl;
+
+	// Assert
+	EXPECT_EQ(check, cCheck.getRe());
+	EXPECT_EQ(check, cCheck.getIm());
+}
