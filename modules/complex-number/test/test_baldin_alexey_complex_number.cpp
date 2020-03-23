ee39d97bc101b1214eb4585a416fed4f4af63724
Alexey <kret906alex@gmail.com>
2020-03-23 23:17:12 +0300

    Fix tabulation again

--- modules/complex-number/test/test_baldin_alexey_complex_number.cpp
+++ modules/complex-number/test/test_baldin_alexey_complex_number.cpp
@@ -34,54 +34,54 @@ TEST(Baldin_Alexey_ComplexNumberTest, Multiple_By_Zero) {
 
 TEST(Baldin_Alexey_ComplexNumberTest, Negative_Less_Then_Positive) {
 	// Arrange
-	double re1 = 123.0;
-	double re2 = -123.0;
-	double im1 = 456.0;
-	double im2 = -456.0;
+    double re1 = 123.0;
+    double re2 = -123.0;
+    double im1 = 456.0;
+    double im2 = -456.0;
 
-	// Act
-	ComplexNumber pos(re1, im1);
-	ComplexNumber neg(re2, im2);
+    // Act
+    ComplexNumber pos(re1, im1);
+    ComplexNumber neg(re2, im2);
 
-	// Assert
-	EXPECT_LT(neg.getRe(), pos.getRe());
-	EXPECT_LT(neg.getIm(), pos.getIm());
+    // Assert
+    EXPECT_LT(neg.getRe(), pos.getRe());
+    EXPECT_LT(neg.getIm(), pos.getIm());
 }
 
 TEST(Baldin_Alexey_ComplexNumberTest, Check_Correct_Math_Operations) {
-	// Arrange
-	double num1 = 10.0;
-	double num2 = 5.0;
+    // Arrange
+    double num1 = 10.0;
+    double num2 = 5.0;
     double num3 = 3.0;
-	double num4 = 2.0;
-	double check = 0;
-	
-	ComplexNumber cNum1(num1, num1);
-	ComplexNumber cNum2(num2, num2);
-	ComplexNumber cNum3(num3, num3);
-	ComplexNumber cNum4(num4, num4);
-	ComplexNumber cCheck(0.0, 0.0);
+    double num4 = 2.0;
+    double check = 0;
 
-	// Act
-	check = num1 + num2 / num3 * num4;
-	cCheck = cNum1 + cNum2 / cNum3 * cNum4;
+    ComplexNumber cNum1(num1, num1);
+    ComplexNumber cNum2(num2, num2);
+    ComplexNumber cNum3(num3, num3);
+    ComplexNumber cNum4(num4, num4);
+    ComplexNumber cCheck(0.0, 0.0);
 
-	// Assert
-	EXPECT_EQ(check, cCheck.getRe());
-	EXPECT_EQ(check, cCheck.getIm());
+    // Act
+    check = num1 + num2 / num3 * num4;
+    cCheck = cNum1 + cNum2 / cNum3 * cNum4;
+
+    // Assert
+    EXPECT_EQ(check, cCheck.getRe());
+    EXPECT_EQ(check, cCheck.getIm());
 }
 
 TEST(Baldin_Alexey_ComplexNumberTest, Check_Inverse_Element_Property) {
-	// Arrange
-	double re = 123.0;
-	double im = 456.0;
-	ComplexNumber pos(re, im);
-	ComplexNumber neg(-re, -im);
+    // Arrange
+    double re = 123.0;
+    double im = 456.0;
+    ComplexNumber pos(re, im);
+    ComplexNumber neg(-re, -im);
 
-	// Act
-	ComplexNumber check = pos + neg;
-	
-	// Assert
-	EXPECT_EQ(check.getRe(), 0.0);
-	EXPECT_EQ(check.getIm(), 0.0);
+    // Act
+    ComplexNumber check = pos + neg;
+
+    // Assert
+    EXPECT_EQ(check.getRe(), 0.0);
+    EXPECT_EQ(check.getIm(), 0.0);
 }
