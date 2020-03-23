d5a631d8dd5d4e957fedbb42fea51b6008fc1da0
Alexey <kret906alex@gmail.com>
2020-03-23 22:17:54 +0300

    Add zero multiple test

--- modules/complex-number/test/test_baldin_alexey_complex_number.cpp
+++ modules/complex-number/test/test_baldin_alexey_complex_number.cpp
@@ -17,3 +17,17 @@ TEST(Baldin_Alexey_ComplexNumberTest, Can_Create_Zero) {
     EXPECT_EQ(im, z.getIm());
 }
 
+TEST(Baldin_Alexey_ComplexNumberTest, Multiple_By_Zero) {
+	// Arrange
+	double re = 123.0;
+	double im = 456.0;
+
+	// Act
+	ComplexNumber z(re, im);
+	ComplexNumber zero(0.0, 0.0);
+	z = z * zero;
+
+	// Assert
+	EXPECT_EQ(z.getRe(), 0.0);
+	EXPECT_EQ(z.getIm(), 0.0);
+}
