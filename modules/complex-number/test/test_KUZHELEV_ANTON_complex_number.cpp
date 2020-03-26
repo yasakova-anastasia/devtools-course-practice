55390792ed8677bb92e9d71891819b5a0c228df5
IconLivin <anton.parole@mail.ru>
2020-03-26 22:17:34 +0300

    Added some tests

--- modules/complex-number/test/test_KUZHELEV_ANTON_complex_number.cpp
+++ modules/complex-number/test/test_KUZHELEV_ANTON_complex_number.cpp
@@ -14,14 +14,14 @@ TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Create_Complex_Number) {
     EXPECT_EQ(im, z.getIm());
 }
 
-TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Create_Deafult_Complex_Number){
+TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Create_Deafult_Complex_Number) {
     ComplexNumber z;
     EXPECT_EQ(0, z.getRe());
     EXPECT_EQ(0, z.getIm());
 }
 
 TEST(Kuzhelev_Anton_ComplexNumberTest, 
-Can_Create_Complex_Number_From_Another_Complex_Number){
+Can_Create_Complex_Number_From_Another_Complex_Number) {
     double re = 12.0;
     double im = 12.0;
 
@@ -32,7 +32,7 @@ Can_Create_Complex_Number_From_Another_Complex_Number){
     EXPECT_EQ(z1, z2);
 }
 
-TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Assign_Complex_Numbers){
+TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Assign_Complex_Numbers) {
     double re = 13.0;
     double im = 13.0;
 
@@ -48,4 +48,49 @@ TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Assign_Complex_Numbers){
     z1 = z2;
 
     EXPECT_EQ(z1, z2);
+}
+
+TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Add_Complex_Numbers) {
+    double re = 15.0;
+    double im = 15.0;
+
+    ComplexNumber z1(re, im);
+
+    re = 16.0;
+    im = 16.0;
+
+    ComplexNumber z2(re, im);
+
+    EXPECT_EQ(z1.getRe() + z2.getRe(), (z1 + z2).getRe());
+    EXPECT_EQ(z1.getIm() + z2.getIm(), (z1 + z2).getIm());
+}
+
+TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Substruct_Complex_Numbers) {
+    double re = 15.0;
+    double im = 15.0;
+
+    ComplexNumber z1(re, im);
+
+    re = 16.0;
+    im = 16.0;
+
+    ComplexNumber z2(re, im);
+
+    EXPECT_EQ(z1.getRe() - z2.getRe(), (z1 - z2).getRe());
+    EXPECT_EQ(z1.getIm() - z2.getIm(), (z1 - z2).getIm());
+}
+
+TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Multiply_Complex_Numbers) {
+    double re = 15.0;
+    double im = 15.0;
+
+    ComplexNumber z1(re, im);
+
+    re = 16.0;
+    im = 16.0;
+
+    ComplexNumber z2(re, im);
+
+    EXPECT_EQ(z1.getRe() * z2.getRe() - z1.getIm() * z2.getIm(), (z1 * z2).getRe());
+    EXPECT_EQ(z1.getRe() * z2.getRe() + z1.getIm() * z2.getIm(), (z1 * z2).getRe());
 }
