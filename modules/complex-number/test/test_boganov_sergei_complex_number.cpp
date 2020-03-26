5a94818bd686416c6192603167cd26dcbd67c54c
Boganov Sergei <frixinglife@gmail.com>
2020-03-26 23:40:36 +0300

    Added 'Can_Subtract_Complex_Numbers' test

--- modules/complex-number/test/test_boganov_sergei_complex_number.cpp
+++ modules/complex-number/test/test_boganov_sergei_complex_number.cpp
@@ -107,3 +107,23 @@ TEST(Boganov_Sergei_ComplexNumberTest, Can_Add_Complex_Numbers) {
     // Assert
     EXPECT_EQ(z1 + z2, z3);
 }
+
+TEST(Boganov_Sergei_ComplexNumberTest, Can_Subtract_Complex_Numbers) {
+    // Arrange
+    double re1 = -3.0;
+    double im1 = 6.0;
+
+    double re2 = 2.0;
+    double im2 = 7.0;
+
+    double re3 = -5.0;
+    double im3 = -1.0;
+
+    // Act
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+    ComplexNumber z3(re3, im3);
+
+    // Assert
+    EXPECT_EQ(z1 - z2, z3);
+}
