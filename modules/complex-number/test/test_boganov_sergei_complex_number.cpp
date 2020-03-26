e550f2b7b5824ba99d22786f815f21aba7d0db40
Boganov Sergei <frixinglife@gmail.com>
2020-03-26 23:50:32 +0300

    Added 'Can_Divide_Complex_Numbers' test

--- modules/complex-number/test/test_boganov_sergei_complex_number.cpp
+++ modules/complex-number/test/test_boganov_sergei_complex_number.cpp
@@ -147,3 +147,23 @@ TEST(Boganov_Sergei_ComplexNumberTest, Can_Multiply_Complex_Numbers) {
     // Assert
     EXPECT_EQ(z1 * z2, z3);
 }
+
+TEST(Boganov_Sergei_ComplexNumberTest, Can_Divide_Complex_Numbers) {
+    // Arrange
+    double re1 = 1.0;
+    double im1 = -1.0;
+
+    double re2 = 1.0;
+    double im2 = 1.0;
+
+    double re3 = 0.0;
+    double im3 = -1.0;
+
+    // Act
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+    ComplexNumber z3(re3, im3);
+
+    // Assert
+    EXPECT_EQ(z1 / z2, z3);
+}
