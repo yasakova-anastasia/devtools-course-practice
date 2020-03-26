d244970e63a1d8fe196864d63f5d9d208934e48d
Boganov Sergei <frixinglife@gmail.com>
2020-03-26 23:09:23 +0300

    Added 'Can_Get_Re' test

--- modules/complex-number/test/test_boganov_sergei_complex_number.cpp
+++ modules/complex-number/test/test_boganov_sergei_complex_number.cpp
@@ -16,3 +16,15 @@ TEST(Boganov_Sergei_ComplexNumberTest, Can_Create_Zero) {
     EXPECT_EQ(re, z.getRe());
     EXPECT_EQ(im, z.getIm());
 }
+
+TEST(Boganov_Sergei_ComplexNumberTest, Can_Get_Re) {
+    // Arrange
+    double re = 5.0;
+    double im = 1.0;
+
+    // Act
+    ComplexNumber z(re, im);
+
+    // Assert
+    EXPECT_EQ(re, z.getRe());
+}
