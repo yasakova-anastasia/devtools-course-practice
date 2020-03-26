67131b4f0856b49a7cf44bca5380b5648ae45e0e
Dybbor <19dobrohotov99@mail.ru>
2020-03-26 19:16:58 +0300

    write 3 tests on constructors

---
+++ modules/complex-number/test/test_Dobrohotov_Vitaly_complex_number.cpp
@@ -0,0 +1,39 @@
+// Copyright 2020 Dobrohotov Vitaly
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+TEST(Dobrohotov_Vitaly_ComplexNumberTest, Check_Default_Constructor) {
+    // Act
+    ComplexNumber z;
+
+    // Assert
+    EXPECT_EQ(0, z.getRe());
+    EXPECT_EQ(0, z.getIm());
+}
+
+TEST(Dobrohotov_Vitaly_ComplexNumberTest, Check_Parameterized_Constructor) {
+	// Arrange
+	double re = 20.0;
+	double im = 5.0;
+
+	// Act
+	ComplexNumber z(re, im);
+
+	// Assert
+	EXPECT_EQ(re, z.getRe());
+	EXPECT_EQ(im, z.getIm());
+}
+
+TEST(Dobrohotov_Vitaly_ComplexNumberTest, Check_Copy_Constructor) {
+	// Arrange
+	ComplexNumber z1(2.0, 4.0);
+
+	// Act
+	ComplexNumber z2(z1);
+
+	// Assert
+	EXPECT_EQ(z1.getRe(), z2.getRe());
+	EXPECT_EQ(z1.getIm(), z2.getIm());
+}
