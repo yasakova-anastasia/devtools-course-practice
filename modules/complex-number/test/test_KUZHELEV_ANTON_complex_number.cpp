2a9f34521c240a04e651f5711776647bcad9226a
IconLivin <anton.parole@mail.ru>
2020-03-26 22:38:43 +0300

    Done job on tests

--- modules/complex-number/test/test_KUZHELEV_ANTON_complex_number.cpp
+++ modules/complex-number/test/test_KUZHELEV_ANTON_complex_number.cpp
@@ -20,7 +20,7 @@ TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Create_Deafult_Complex_Number) {
     EXPECT_EQ(0, z.getIm());
 }
 
-TEST(Kuzhelev_Anton_ComplexNumberTest, 
+TEST(Kuzhelev_Anton_ComplexNumberTest,
 Can_Create_Complex_Number_From_Another_Complex_Number) {
     double re = 12.0;
     double im = 12.0;
@@ -91,6 +91,30 @@ TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Multiply_Complex_Numbers) {
 
     ComplexNumber z2(re, im);
 
-    EXPECT_EQ(z1.getRe() * z2.getRe() - z1.getIm() * z2.getIm(), (z1 * z2).getRe());
-    EXPECT_EQ(z1.getRe() * z2.getRe() + z1.getIm() * z2.getIm(), (z1 * z2).getRe());
+    EXPECT_EQ(z1.getRe() * z2.getRe() - z1.getIm() * z2.getIm(),
+    (z1 * z2).getRe());
+    EXPECT_EQ(z1.getRe() * z2.getRe() + z1.getIm() * z2.getIm(),
+    (z1 * z2).getIm());
+}
+
+TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Compare_Equal_Complex_Numbers) {
+    double re = 20.0;
+    double im = 20.0;
+
+    ComplexNumber z1(re, im);
+
+    ComplexNumber z2(re, im);
+
+    EXPECT_TRUE(z1 == z2);
+}
+
+TEST(Kuzhelev_Anton_ComplexNumberTest, Can_Compare_Not_Equal_Complex_Numbers) {
+    double re = 20.0;
+    double im = 20.0;
+
+    ComplexNumber z1(re, im);
+
+    ComplexNumber z2(re + 1, im + 1);
+
+    EXPECT_TRUE(z1 != z2);
 }
