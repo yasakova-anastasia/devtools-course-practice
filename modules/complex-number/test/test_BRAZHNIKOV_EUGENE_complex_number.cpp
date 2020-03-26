10ef680cdcce5a48c331dd80d4d63231c42b4417
Anorak52 <siber56@yandex.ru>
2020-03-26 11:48:37 +0300

    All test is work

--- modules/complex-number/test/test_BRAZHNIKOV_EUGENE_complex_number.cpp
+++ modules/complex-number/test/test_BRAZHNIKOV_EUGENE_complex_number.cpp
@@ -19,9 +19,9 @@ TEST(Brazhnikov_Eugene_ComplexNumberTest, Self_Assign) {
 TEST(Brazhnikov_Eugene_ComplexNumberTest, Multiply_6_times_in_a_row) {
     // Arrange
     ComplexNumber CN_1(2.0, 4.0);
-    ComplexNumber CN_2(5.0, 2.0);  //2+24i
-    ComplexNumber CN_3(10.0, 5.0);  //-100+250i
-    ComplexNumber CN_4(1.0, 2.0);  //-400+450i
+    ComplexNumber CN_2(5.0, 2.0);  // 2+24i
+    ComplexNumber CN_3(10.0, 5.0);  // -100+250i
+    ComplexNumber CN_4(1.0, 2.0);  // -400+450i
     ComplexNumber CN_5(400.0, 450.0);
     ComplexNumber CN_6(1.0, 2.0);
     ComplexNumber expected_CN(237500.0, -775000.0);
