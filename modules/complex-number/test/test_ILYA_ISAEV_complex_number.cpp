a530ab01bcfaa85c7e39eea8c32c315e63531da7
buzyaba <isaev.ameninc@gmail.com>
2020-03-26 16:17:55 +0300

    Added first test

--- modules/complex-number/test/test_ILYA_ISAEV_complex_number.cpp
+++ modules/complex-number/test/test_ILYA_ISAEV_complex_number.cpp
@@ -6,3 +6,9 @@
 
 #include "include/complex_number.h"
 
+TEST(Ilya_Isaev_Complex_Number_Test, Create_Instance_With_Params) {
+    auto a = ComplexNumber(1.05, 2.15);
+
+    EXPECT_EQ(1.05, a.getRe());
+    EXPECT_EQ(2.15, a.getIm());
+}
