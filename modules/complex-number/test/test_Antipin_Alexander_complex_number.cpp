b6d1568121cc6f3ca727bfa98985e9e2bc607da2
Alex Frog <aleksandr.antipin.1999@mail.ru>
2020-03-27 14:28:44 +0300

    some correction

--- modules/complex-number/test/test_Antipin_Alexander_complex_number.cpp
+++ modules/complex-number/test/test_Antipin_Alexander_complex_number.cpp
@@ -4,11 +4,13 @@
 
 #include "include/complex_number.h"
 
-TEST(Antipin_Alexander_ComplexNumberTest, can_create_complex_number_without_throws) {
+TEST(Antipin_Alexander_ComplexNumberTest,
+    can_create_complex_number_without_throws) {
     ASSERT_NO_THROW(ComplexNumber(1.0, 2.0));
 }
 
-TEST(Antipin_Alexander_ComplexNumberTest, can_create_complex_number_correct_1) {
+TEST(Antipin_Alexander_ComplexNumberTest,
+    can_create_complex_number_correct_1) {
     double re{};
     double im{ 1.0 };
 
@@ -17,7 +19,8 @@ TEST(Antipin_Alexander_ComplexNumberTest, can_create_complex_number_correct_1) {
     EXPECT_EQ(0.0, nomber.getRe());
 }
 
-TEST(Antipin_Alexander_ComplexNumberTest, can_create_complex_number_correct_2) {
+TEST(Antipin_Alexander_ComplexNumberTest,
+    can_create_complex_number_correct_2) {
     double re{};
     double im{ 1.0 };
 
