582f50386f287ecfe98c336b3c0716a2480628f5
Rave Guy <Rave Guy>
2020-03-27 11:02:36 +0300

    fix

--- modules/complex-number/test/test_suchkov_makar_complex_number.cpp
+++ modules/complex-number/test/test_suchkov_makar_complex_number.cpp
@@ -75,8 +75,8 @@ TEST(Suchkov_Makar_ComplexNumberTest, Negative_Number_Less_Than_Positive) {
 
 TEST(Suchkov_Makar_ComplexNumberTest, Can_Add_Complex_Numbers) {
     // Arrange
-    double re1 = 5.0;
-    double im1 = 9.0;
+    double re1 = 1.0;
+    double im1 = 2.0;
 
     double re2 = 4.0;
     double im2 = -5.0;
