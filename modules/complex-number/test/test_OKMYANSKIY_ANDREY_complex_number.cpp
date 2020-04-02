00d73ba743e74400ccd82d7e200149aa5634c366
Андрей <okm.andrey@yandex.ru>
2020-04-02 17:22:52 +0300

    initial commit

---
+++ modules/complex-number/test/test_OKMYANSKIY_ANDREY_complex_number.cpp
@@ -0,0 +1,84 @@
+// Copyright 2020 Okmyanskiy Andrey
+
+#include <gtest/gtest.h>
+#include "include/complex_number.h"
+
+TEST(Okmyanskiy_Andrey_ComplexNumberTest, Multiply_Complex_And_Zero) {
+    // Arrange
+    double re = 15.0;
+    double im = 2.0;
+    ComplexNumber z(re, im);
+    ComplexNumber complexZero(0.0, 0.0);
+    ComplexNumber testZero;
+    
+    // Act
+    testZero = z * complexZero;
+
+    // Assert
+    EXPECT_EQ(complexZero.getRe(), testZero.getRe());
+    EXPECT_EQ(complexZero.getIm(), testZero.getIm());
+}
+
+TEST(Okmyanskiy_Andrey_ComplexNumberTest, Addition_Complex_And_Zero) {
+    // Arrange
+    double re = 15.0;
+    double im = 2.0;
+    ComplexNumber z(re, im);
+    ComplexNumber complexZero(0.0, 0.0);
+    ComplexNumber testZero;
+    
+    // Act
+    testZero = z + complexZero;
+
+    // Assert
+    EXPECT_EQ(z.getRe(), testZero.getRe());
+    EXPECT_EQ(z.getIm(), testZero.getIm());
+}
+
+TEST(Okmyanskiy_Andrey_ComplexNumberTest, Difference_Complex_And_Zero) {
+    // Arrange
+    double re = 15.0;
+    double im = 2.0;
+    ComplexNumber z(re, im);
+    ComplexNumber complexZero(0.0, 0.0);
+    ComplexNumber testZero;
+    
+    // Act
+    testZero = z - complexZero;
+
+    // Assert
+    EXPECT_EQ(z.getRe(), testZero.getRe());
+    EXPECT_EQ(z.getIm(), testZero.getIm());
+}
+
+TEST(Okmyanskiy_Andrey_ComplexNumberTest, Division_Complex_And_Zero) {
+    // Arrange
+    double re = 15.0;
+    double im = 2.0;
+    ComplexNumber z(re, im);
+    ComplexNumber complexZero(0.0, 0.0);
+    ComplexNumber testZero;
+    
+    // Act
+    testZero = complexZero / z;
+
+    // Assert
+    EXPECT_EQ(complexZero.getRe(), testZero.getRe());
+    EXPECT_EQ(complexZero.getIm(), testZero.getIm());
+}
+
+TEST(Okmyanskiy_Andrey_ComplexNumberTest, Multiply_Complex_And_One) {
+    // Arrange
+    double re = 15.0;
+    double im = 2.0;
+    ComplexNumber z(re, im);
+    ComplexNumber complexOne(1.0, 0.0);
+    ComplexNumber testOne;
+    
+    // Act
+    testOne = complexOne * z;
+
+    // Assert
+    EXPECT_EQ(z.getRe(), testOne.getRe());
+    EXPECT_EQ(z.getIm(), testOne.getIm());
+}
