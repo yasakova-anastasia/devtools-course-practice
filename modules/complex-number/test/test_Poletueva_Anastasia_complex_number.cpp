ecbe0338d1d5180768dbcee00b8224da2c6b5631
381706AnastasiaPoletueva <nastenka1193@mail.ru>
2020-04-03 20:51:11 +0300

    Correction

--- modules/complex-number/test/test_Poletueva_Anastasia_complex_number.cpp
+++ modules/complex-number/test/test_Poletueva_Anastasia_complex_number.cpp
@@ -4,7 +4,7 @@
 
 #include "include/complex_number.h"
 
-TEST(Poletueva_Anastasia_ComplexNumberTest, Can_Create_With_Negative_Real_And_Imaginary) {
+TEST(Poletueva_Anastasia_ComplexNumberTest, Can_Create_With_Negative_Re_And_Im) {
     // Arrange
     double re = -1.2;
     double im = -5.3;
@@ -17,25 +17,5 @@ TEST(Poletueva_Anastasia_ComplexNumberTest, Can_Create_With_Negative_Real_And_Im
     EXPECT_EQ(im, z.getIm());
 }
 
-TEST(Poletueva_Anastasia_ComplexNumberTest, Can_Ñomposition_By_Zero) {
-  // Arrange
-  ComplexNumber z(8.2, 4.5);
-  ComplexNumber k(0, 0);
 
-  // Act
-  ComplexNumber p = z*k;
-
-  // Act & Assert
-  EXPECT_EQ(0, p.getRe());
-  EXPECT_EQ(0, p.getIm());
-}
-
-TEST(Poletueva_Anastasia_ComplexNumberTest, Can_Division_By_Zero) {
-  // Arrange
-  ComplexNumber z(1.2, 3.5);
-  ComplexNumber k(0, 0);
-
-  // Act & Assert
-  ASSERT_ANY_THROW(z / k);
-}
 
