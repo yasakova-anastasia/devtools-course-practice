420d9b7bf95e4c6f564bf666c6bacef03bf90d05
381706AnastasiaPoletueva <nastenka1193@mail.ru>
2020-04-03 20:59:05 +0300

    Added 2 tests

--- modules/complex-number/test/test_Poletueva_Anastasia_complex_number.cpp
+++ modules/complex-number/test/test_Poletueva_Anastasia_complex_number.cpp
@@ -17,5 +17,24 @@ TEST(Poletueva_Anastasia_ComplexNumberTest, Can_Create_With_Negative_Re_And_Im) 
     EXPECT_EQ(im, z.getIm());
 }
 
+TEST(Poletueva_Anastasia_ComplexNumberTest, Can_Ñomposition_By_Zero) {
+  // Arrange
+  ComplexNumber z(8.2, 4.5);
+  ComplexNumber k(0, 0);
 
+  // Act
+  ComplexNumber p = z*k;
 
+  // Act & Assert
+  EXPECT_EQ(0, p.getRe());
+  EXPECT_EQ(0, p.getIm());
+}
+
+TEST(Poletueva_Anastasia_ComplexNumberTest, Can_Division_By_Zero) {
+  // Arrange
+  ComplexNumber z(1.2, 3.5);
+  ComplexNumber k(0, 0);
+
+  // Act & Assert
+  ASSERT_ANY_THROW(z / k);
+}
