8b298d36a0bd23e316845fc60ab7143ef744abe5
tanskiiui <yurko1337@gmail.com>
2020-04-03 18:55:13 +0300

    Add more Tests

--- modules/complex-number/test_TANSKII_YURII_complex_number.cpp
+++ modules/complex-number/test_TANSKII_YURII_complex_number.cpp
@@ -13,7 +13,6 @@ TEST(Tanskii_Yurii_ComplexNumberTest, Can_Multi) {
     double ans_re = 59.0;
     double ans_im = -41.0;
 
-
     // Act
     ComplexNumber z1(re1, im1);
     ComplexNumber z2(re2, im2);
@@ -23,5 +22,53 @@ TEST(Tanskii_Yurii_ComplexNumberTest, Can_Multi) {
     // Assert
     EXPECT_EQ(ans_re, z3.getRe());
     EXPECT_EQ(ans_im, z3.getIm());
+}
 
+TEST(Tanskii_Yurii_ComplexNumberTest, Can_sub) {
+    // Arrange
+    double re1 = -4.0;
+    double im1 = 16.0;
+    double re2 = 11.0;
+    double im2 = -8.0;
+    double ans_re = -15.0;
+    double ans_im = 24.0;
+
+    // Act
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+    ComplexNumber z3(0.0, 0.0);
+    z3 = z1 - z2;
+
+    // Assert
+    EXPECT_EQ(ans_re, z3.getRe());
+    EXPECT_EQ(ans_im, z3.getIm());
+}
+
+TEST(Tanskii_Yurii_ComplexNumberTest, Could_except_divide_by_zero) {
+    // Arrange
+    double re1 = -4.0;
+    double im1 = 16.0;
+    
+    // Act
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2;
+  
+    // Assert
+    ASSERT_ANY_THROW(z1 / z2);
+}
+
+TEST(Tanskii_Yurii_ComplexNumberTest, Can_sum) {
+    // Arrange
+    double re1 = -4.0;
+    double im1 = 16.0;
+    double re2 = 4.0;
+    double im2 = -16.0;
+
+    // Act
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+    ComplexNumber z3;
+
+    // Assert
+    ASSERT_ANY_THROW(z3, z1 + z2);
 }
