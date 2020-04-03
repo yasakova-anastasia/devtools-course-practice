ba7dd5a10653130ab00778959a05d9bd86a652fb
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2020-04-03 18:17:54 +0300

    Added more tests

--- modules/complex-number/test/test_golubeva_anna_complex_number.cpp
+++ modules/complex-number/test/test_golubeva_anna_complex_number.cpp
@@ -30,6 +30,41 @@ TEST(Golubeva_Anna_ComplexNumberTest, Copy_Constructor) {
   EXPECT_EQ(num1, num2);
 }
 
+TEST(Golubeva_Anna_ComplexNumberTest, Can_Add) {
+  // Arrange
+  ComplexNumber num1(5.0, 4.0);
+  ComplexNumber num2(2.2, 1.2);
+
+  // Act
+  ComplexNumber num3 = num1 + num2;
+
+  // Assert
+  EXPECT_EQ(num3, ComplexNumber(7.2, 5.2));
+}
+
+TEST(Golubeva_Anna_ComplexNumberTest, Can_Substract_Complex_Number) {
+// Arrange
+  ComplexNumber num1(2.2, 3.3);
+  ComplexNumber num2(1.1, -3.3);
+
+  // Act
+  ComplexNumber num3 = num1 - num2;
+
+  // Assert
+  EXPECT_EQ(num3, ComplexNumber(1.1, 6.6));
+}
+
+TEST(Golubeva_Anna_ComplexNumberTest, Division_By_Zero) {
+// Arrange
+  ComplexNumber num1(1.1, 2.2);
+  ComplexNumber num2;
+
+  // Act & Assert
+  ASSERT_ANY_THROW(num1 / num2);
+}
+
+
+
 
 
 
