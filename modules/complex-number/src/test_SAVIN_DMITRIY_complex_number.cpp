815a0289911c8077963a66f69a90e6ee58858f8e
Dron228 <savin.duma@yandex.ru>
2020-04-04 01:09:38 +0300

    Add Test2

--- modules/complex-number/src/test_SAVIN_DMITRIY_complex_number.cpp
+++ modules/complex-number/src/test_SAVIN_DMITRIY_complex_number.cpp
@@ -24,14 +24,3 @@ TEST(Savin_Dmitriy_ComplexNumberTest, Test_Number_On_Than_comparison_less) {
     EXPECT_LT(re, num2.getRe());
     EXPECT_LT(im, num2.getIm());
 }
-TEST(Savin_Dmitriy_ComplexNumberTest, Test_Number_On_Than_comparison) {
-    double re = 4.0;
-    double im = 3.0;
-    double re1 = 2.0;
-    double im2 = 1.0;
-
-    ComplexNumber num2(re, im);
-
-    ASSERT_GE(re, num2.getRe());
-    ASSERT_GE(im, num2.getIm());
-}
