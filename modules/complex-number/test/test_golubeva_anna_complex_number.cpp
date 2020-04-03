f4af6d48fe30d1a8b86ed6c0e1a0dbfd2421be7f
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2020-04-03 15:49:15 +0300

    Added new test

--- modules/complex-number/test/test_golubeva_anna_complex_number.cpp
+++ modules/complex-number/test/test_golubeva_anna_complex_number.cpp
@@ -4,6 +4,21 @@
 
 #include "include/complex_number.h"
 
+TEST(Golubeva_Anna_ComplexNumberTest, Can_Set_Real_And_Imaginary) {
+// Arrange
+  ComplexNumber num;
+  double real = 2.2;
+  double imaginary = 1.1;
+
+  // Act
+  num.setRe(real);
+  num.setIm(imaginary);
+
+  // Assert
+  ASSERT_EQ(real, num.getRe());
+  ASSERT_EQ(imaginary, num.getIm());
+}
+
 TEST(Golubeva_Anna_ComplexNumberTest, Copy_Constructor) {
   // Arrange
   ComplexNumber num1(3.3, 2.2);
@@ -14,3 +29,7 @@ TEST(Golubeva_Anna_ComplexNumberTest, Copy_Constructor) {
   // Assert
   EXPECT_EQ(num1, num2);
 }
+
+
+
+
