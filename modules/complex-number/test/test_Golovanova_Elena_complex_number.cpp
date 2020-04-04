72f0bd90d396fc9823c3a0d8609ab1c91117321a
Lena381706-1 <gol.elena99@gmail.com>
2020-04-04 22:24:41 +0300

    Added 3 test for lab1

--- modules/complex-number/test/test_Golovanova_Elena_complex_number.cpp
+++ modules/complex-number/test/test_Golovanova_Elena_complex_number.cpp
@@ -30,3 +30,15 @@ TEST(Golovanova_Elena_ComplexNumberTest, Can_Create_Addition) {
   EXPECT_EQ(8.5, z3.getIm());
 }
 
+TEST(Golovanova_Elena_ComplexNumberTest, Can_Create_Subtraction) {
+  // Arrange
+  ComplexNumber z1(5.0, 4.0);
+  ComplexNumber z2(2.0, 1.0);
+
+  // Act
+  ComplexNumber z3 = z1 - z2;
+
+  // Assert
+  EXPECT_EQ(3.0, z3.getRe());
+  EXPECT_EQ(3.0, z3.getIm());
+}
