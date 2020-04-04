3defd0a5801a19a494af072970f375ee61de6f5e
ArisovaAnastasia <nasaris2406@gmail.com>
2020-04-04 16:50:30 +0300

    Added new tests for lab1

--- modules/complex-number/test/test_Arisova_Anastasia_complex_number.cpp
+++ modules/complex-number/test/test_Arisova_Anastasia_complex_number.cpp
@@ -16,3 +16,28 @@ TEST(Arisova_Anastasiia_ComplexNumberTest, Can_Create_Zero) {
     EXPECT_EQ(re, z.getRe());
     EXPECT_EQ(im, z.getIm());
 }
+
+TEST(Arisova_Anastasiia_ComplexNumberTest, Can_Multiplication_Complex_and_Zero) {
+    // Arrange
+    ComplexNumber z1(2.0, 5.4);
+    ComplexNumber z2(0.0, 0.0);
+
+    // Act
+    ComplexNumber z = z1 * z2;
+
+    // Assert
+    ComplexNumber answer(0.0, 0.0);
+    EXPECT_EQ(answer, z);
+}
+
+TEST(Arisova_Anastasiia_ComplexNumberTest, Can_Multiplication_By_Itself) {
+    // Arrange
+    ComplexNumber z1(1.0, 2.0);
+
+    // Act
+    ComplexNumber z = z1 * z1;
+
+    // Assert
+    ComplexNumber answer(-3.0, 4.0);
+    EXPECT_EQ(answer, z);
+}
