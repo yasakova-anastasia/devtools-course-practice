242722065b0fbb8528b835e898023803dd35b966
IconLivin <anton.parole@mail.ru>
2020-04-07 13:33:35 +0300

    Trevis failure fix

--- modules/prime_numbers/src/TPrime_Nums.cpp
+++ modules/prime_numbers/src/TPrime_Nums.cpp
@@ -13,7 +13,7 @@ TPrime_Nums::TPrime_Nums(int left = 0, int right = 0) {
 }
 
 void TPrime_Nums::SetInterval(std::pair<int, int> interval) {
-    if (interval.first < 0 || interval.second < 0 
+    if (interval.first < 0 || interval.second < 0
         || interval.first > interval.second) {
         throw -1;
     }
