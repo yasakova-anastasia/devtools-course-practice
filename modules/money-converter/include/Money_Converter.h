f987f95a3c3649bff25154214ddeb7a4fa3f7e59
Anorak52 <siber56@yandex.ru>
2020-04-07 13:01:21 +0300

    Upd class

--- modules/money-converter/include/Money_Converter.h
+++ modules/money-converter/include/Money_Converter.h
@@ -5,54 +5,22 @@
 
 class MoneyConverter {
  public:
-    double rubdollar(const double& curr, int  WhichOne) {
-        if (curr >= 0 && WhichOne == 0) {  // RUB to EUR
-            return curr / 77.73;
-        } else if (curr >= 0 && WhichOne == 1) {  // EUR to RUB
-            return curr * 77.73;
-        } else {
-            return -1;
-        }
-    }
-
-    double rubeur(const double& curr, int WhichOne) {
-        if (curr >= 0 && WhichOne == 0) {
-            return curr / 85.73;
-        } else if (curr >= 0 && WhichOne == 1) {
-            return curr * 85.73;
-        } else {
-            return -1;
-        }
-    }
-
-    double rubJPY(const double& curr, int WhichOne) {
-        if (curr >= 0 && WhichOne == 0) {
-            return curr / 71.40;
-        } else if (curr >= 0 && WhichOne == 1) {
-            return curr * 71.40;
-        } else {
-            return -1;
-        }
-    }
-
-    double rubSEK(const double& curr, int WhichOne) {
-        if (curr >= 0 && WhichOne == 0) {
-            return curr / 78;
-        } else if (curr >= 0 && WhichOne == 1) {
-            return curr * 78;
-        } else {
-            return -1;
-        }
-    }
-
-    double rubUAH(const double& curr, int WhichOne) {
-        if (curr >= 0 && WhichOne == 0) {
-            return curr / 2.79;
-        } else if (curr >= 0 && WhichOne == 1) {
-            return curr * 2.79;
-        } else { return -1;
-        }
-    }
+     MoneyConverter() {
+         curr = 0;
+         WhichOne = 0;
+     }
+     MoneyConverter(double currency, int amount) {
+         curr = currency;
+         WhichOne = amount;
+     }
+     double rubdollar(const double& curr, int WhichOne);
+     double rubeur(const double& curr, int WhichOne);
+     double rubJPY(const double& curr, int WhichOne);
+     double rubSEK(const double& curr, int WhichOne);
+     double rubUAH(const double& curr, int WhichOne);
+ private:
+    double curr;
+    int WhichOne;
 };
 
 #endif  // MODULES_MONEY_CONVERTER_INCLUDE_MONEY_CONVERTER_H_
--- modules/money-converter/src/Money_Converter.cpp
+++ modules/money-converter/src/Money_Converter.cpp
@@ -1,3 +1,63 @@
 // Copyright 2020 Brazhnikov Eugene
 
 #include "include/Money_Converter.h"
+
+double MoneyConverter::rubdollar(const double& curr, int  WhichOne) {
+    if (curr >= 0 && WhichOne == 0) {  // RUB to EUR
+        return curr / 77.73;
+    }
+    else if (curr >= 0 && WhichOne == 1) {  // EUR to RUB
+        return curr * 77.73;
+    }
+    else {
+        return -1;
+    }
+}
+
+double MoneyConverter::rubeur(const double& curr, int WhichOne) {
+    if (curr >= 0 && WhichOne == 0) {
+        return curr / 85.73;
+    }
+    else if (curr >= 0 && WhichOne == 1) {
+        return curr * 85.73;
+    }
+    else {
+        return -1;
+    }
+}
+
+double MoneyConverter::rubJPY(const double& curr, int WhichOne) {
+    if (curr >= 0 && WhichOne == 0) {
+        return curr / 71.40;
+    }
+    else if (curr >= 0 && WhichOne == 1) {
+        return curr * 71.40;
+    }
+    else {
+        return -1;
+    }
+}
+
+double MoneyConverter::rubSEK(const double& curr, int WhichOne) {
+    if (curr >= 0 && WhichOne == 0) {
+        return curr / 78;
+    }
+    else if (curr >= 0 && WhichOne == 1) {
+        return curr * 78;
+    }
+    else {
+        return -1;
+    }
+}
+
+double MoneyConverter::rubUAH(const double& curr, int WhichOne) {
+    if (curr >= 0 && WhichOne == 0) {
+        return curr / 2.79;
+    }
+    else if (curr >= 0 && WhichOne == 1) {
+        return curr * 2.79;
+    }
+    else {
+        return -1;
+    }
+}
