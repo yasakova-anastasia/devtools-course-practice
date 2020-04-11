1d2578a93e7f11fba2c5042dc432248efd9f79c5
Mazur Daniil <mazurdany@yandex.ru>
2020-04-12 01:03:42 +0300

    Fix and add tests

--- modules/triangle_param/test/test_triangle_param.cpp
+++ modules/triangle_param/test/test_triangle_param.cpp
@@ -21,12 +21,116 @@ TEST(TriangleParamTest, EmptyRest) {
 
 TEST(TriangleParamTest, DefaultComstructor) {
   // Arrange
-  Triangle triag();
+  Triangle triag{};
+  std::pair<double, double> expexted (0, 0);
   // Act
-  std::pair <double, double> a = triag().getTopA();
+
   // Assert
-  ASSERT_EQ((0, 0), triag().getTopA());
-  ASSERT_EQ((0, 0), triag().getTopB());
-  ASSERT_EQ((0, 0), triag().getTopB());
-  ASSERT_EQ(0, 0);
+  ASSERT_EQ(expexted, triag.getTopA());
+  ASSERT_EQ(expexted, triag.getTopB());
+  ASSERT_EQ(expexted, triag.getTopC());
+}
+
+TEST(TriangleParamTest, ComstructorByArgs) {
+  // Arrange
+  std::pair<double, double> expextedA(0, 0);
+  std::pair<double, double> expextedB(2, 4);
+  std::pair<double, double> expextedC(10, 3);
+  Triangle triag( expextedA, expextedB, expextedC);
+
+  // Act
+
+  // Assert
+  ASSERT_EQ(expextedA, triag.getTopA());
+  ASSERT_EQ(expextedB, triag.getTopB());
+  ASSERT_EQ(expextedC, triag.getTopC());
+}
+
+TEST(TriangleParamTest, ComstructorByCopy) {
+  // Arrange
+  std::pair<double, double> expextedA(0, 0);
+  std::pair<double, double> expextedB(2, 4);
+  std::pair<double, double> expextedC(10, 3);
+  Triangle triag(expextedA, expextedB, expextedC);
+  Triangle triag2(triag);
+
+  // Act
+
+  // Assert
+  ASSERT_EQ(triag.getTopA(), triag2.getTopA());
+  ASSERT_EQ(triag.getTopB(), triag2.getTopB());
+  ASSERT_EQ(triag.getTopC(), triag2.getTopC());
+}
+
+TEST(TriangleParamTest, SetTops) {
+  // Arrange
+  std::pair<double, double> expextedA(0, 0);
+  std::pair<double, double> expextedB(2, 4);
+  std::pair<double, double> expextedC(10, 3);
+  Triangle triag{};
+
+  // Act
+  triag.setTopA(expextedA);
+  triag.setTopB(expextedB);
+  triag.setTopC(expextedC);
+  // Assert
+  ASSERT_EQ(expextedA, triag.getTopA());
+  ASSERT_EQ(expextedB, triag.getTopB());
+  ASSERT_EQ(expextedC, triag.getTopC());
+}
+
+TEST(TriangleParamTest, AssignOverloaded) {
+  // Arrange
+  std::pair<double, double> expextedA(0, 0);
+  std::pair<double, double> expextedB(2, 4);
+  std::pair<double, double> expextedC(10, 3);
+  Triangle triag(expextedA, expextedB, expextedC);
+
+  // Act
+  Triangle triag2 = triag;
+  // Assert
+  ASSERT_EQ(triag.getTopA(), triag2.getTopA());
+  ASSERT_EQ(triag.getTopB(), triag2.getTopB());
+  ASSERT_EQ(triag.getTopC(), triag2.getTopC());
+}
+
+TEST(TriangleParamTest, EqOverloaded) {
+  // Arrange
+  std::pair<double, double> expextedA(0, 0);
+  std::pair<double, double> expextedB(2, 4);
+  std::pair<double, double> expextedC(10, 3);
+  Triangle triag(expextedA, expextedB, expextedC);
+
+  // Act
+  Triangle triag2 = triag;
+  // Assert
+  ASSERT_TRUE(triag == triag2);
+}
+
+TEST(TriangleParamTest, NoEqOverloaded) {
+  // Arrange
+  std::pair<double, double> expextedA(0, 0);
+  std::pair<double, double> expextedB(2, 4);
+  std::pair<double, double> expextedC(10, 3);
+  Triangle triag(expextedA, expextedB, expextedC);
+
+  // Act
+  Triangle triag2{};
+  // Assert
+  ASSERT_FALSE(triag == triag2);
+}
+
+TEST(TriangleParamTest, Perimetr) {
+  // Arrange
+  std::pair<double, double> expextedA(0, 0);
+  std::pair<double, double> expextedB(2, 4);
+  std::pair<double, double> expextedC(10, 3);
+  Triangle triag(expextedA, expextedB, expextedC);
+
+  // Act
+  double AB = triag.side(triag.getTopA(), triag.getTopB());
+  double BC = triag.side(triag.getTopC(), triag.getTopB());
+  double AC = triag.side(triag.getTopA(), triag.getTopC());
+  // Assert
+  ASSERT_EQ(triag.perimetr(), AB + BC + AC);
 }
