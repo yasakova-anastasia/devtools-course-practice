93f444e713d2fc562117a9ba25da7149a190b4e9
11asa11 <serega_kozm@mail.ru>
2020-04-12 23:03:42 +0300

    fixed tabs

--- modules/converter/include/converter.h
+++ modules/converter/include/converter.h
@@ -1,22 +1,23 @@
+// Copyright 2020 Andreev Sergey
+
 #ifndef MODULES_CONVERTER_INCLUDE_CONVERTER_H_
 #define MODULES_CONVERTER_INCLUDE_CONVERTER_H_
 
-class Convert
-{
-private:
+class Convert {
+ private:
 
-	double num_to_Kelvins = 273.15;
-	double num_to_Newton = 0.33;
+double num_to_Kelvins = 273.15;
+double num_to_Newton = 0.33;
 
-public:
+ public:
 
-	Convert() {};
+    Convert() {};
 
-	double convert_to_Kelvins(const double& celsius);
+    double convert_to_Kelvins(const double& celsius);
 
-	double convert_to_Fahrenheit(const double& celsius);
+    double convert_to_Fahrenheit(const double& celsius);
 
-	double convert_to_Newton(const double& celsius);
+    double convert_to_Newton(const double& celsius);
 };
 
-#endif
+#endif // MODULES_CONVERTER_INCLUDE_CONVERTER_H_
