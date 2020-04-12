3c8782f28a4ce01726eb9135ec06f7d7541eaa6a
11asa11 <serega_kozm@mail.ru>
2020-04-12 23:54:56 +0300

    Fixed[1]

--- modules/converter/src/converter.cpp
+++ modules/converter/src/converter.cpp
@@ -1,18 +1,15 @@
+// Copyright 2020 Andreev Sergey
+
 #include "include/converter.h"
-#include <iostream>
 
-double Convert::convert_to_Kelvins(const double& celsius)
-{
-    return (num_to_Kelvins + celsius);
+double Convert::convert_to_Kelvins(const double& celsius) {
+return (num_to_Kelvins + celsius);
 }
 
-double Convert::convert_to_Fahrenheit(const double& celsius)
-{
-    return (celsius * 1.8 + 32);
+double Convert::convert_to_Fahrenheit(const double& celsius) {
+return (celsius * 1.8 + 32);
 }
 
-
-double Convert::convert_to_Newton(const double& celsius)
-{
-    return (num_to_Newton * celsius);
+double Convert::convert_to_Newton(const double& celsius) {
+return (num_to_Newton * celsius);
 }
