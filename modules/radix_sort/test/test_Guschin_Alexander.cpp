55f4e4ac724d0155c5239f4fbc4c7b868065894e
Alexander Guschin <igushin1999@gmail.com>
2020-04-16 19:14:16 +0300

    fix for converse

--- modules/radix_sort/test/test_Guschin_Alexander.cpp
+++ modules/radix_sort/test/test_Guschin_Alexander.cpp
@@ -19,7 +19,7 @@ TEST(RadixSortTest, Can_Find_Unsorted_Array) {
 
   bool result = IsSorted(vec);
 
-  EXPECT_EQ(false, result);
+  EXPECT_EQ(static_cast<bool>(false), result);
 }
 
 TEST(RadixSortTest, Can_Find_Sorted_Array) {
@@ -27,7 +27,7 @@ TEST(RadixSortTest, Can_Find_Sorted_Array) {
 
   bool result = IsSorted(vec);
 
-  EXPECT_EQ(true, result);
+  EXPECT_EQ(static_cast<bool> (true), result);
 }
 
 TEST(RadixSortTest, Can_Sort_Sorted_Array) {
