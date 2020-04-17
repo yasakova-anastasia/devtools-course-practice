f1de47b90faa76b44743ab18c774df2f860360ed
buzyaba <isaev.ameninc@gmail.com>
2020-04-17 19:23:56 +0300

    Capacity constructor

--- modules/hashmap/include/hashmap.h
+++ modules/hashmap/include/hashmap.h
@@ -1,6 +1,26 @@
 // Copyright 2020 Isaev Ilya
 
+#include <initializer_list>
+#include <utility>
+
 template<typename Key=int, typename Value=int>
 class hashmap {
-
+    using value_type = std::pair<Key, Value>;
+    public:
+        explicit hashmap() = default;
+        hashmap(const int& max_size);
+        hashmap(std::initializer_list<value_type> init);
+        int max_size() {return _max_size;}
+    private:
+        int _max_size;
 };
+
+template <typename Key, typename Value>
+hashmap<Key, Value>::hashmap(const int& max_size):_max_size(max_size) {
+
+}
+
+template <typename Key, typename Value>
+hashmap<Key, Value>::hashmap(std::initializer_list<value_type> init) {
+
+}
--- modules/hashmap/test/test_hashmap.cpp
+++ modules/hashmap/test/test_hashmap.cpp
@@ -11,3 +11,13 @@ TEST(HashMapTest, can_create_hash_map) {
 TEST(HashMapTest, can_create_hash_with_template_params) {
     ASSERT_NO_THROW((hashmap<int, double>()));
 }
+
+TEST(HashMapTest, hashmap_from_initializer_list) {
+    ASSERT_NO_THROW((hashmap<int, double>({{1, 2.5}, {0, 3.4}})));
+}
+
+TEST(HashMapTest, can_get_elems_max_size) {
+    hashmap<std::string, double> map(10);
+    
+    ASSERT_EQ(10, map.max_size());
+}
