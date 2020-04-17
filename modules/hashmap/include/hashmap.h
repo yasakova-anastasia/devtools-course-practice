b786da6f4447bd2e174a4b86aca3f22dc0cea741
buzyaba <isaev.ameninc@gmail.com>
2020-04-17 18:20:34 +0300

    Declared as template

--- modules/hashmap/include/hashmap.h
+++ modules/hashmap/include/hashmap.h
@@ -1,5 +1,6 @@
 // Copyright 2020 Isaev Ilya
 
+template<typename Key=int, typename Value=int>
 class hashmap {
 
 };
--- modules/hashmap/test/test_hashmap.cpp
+++ modules/hashmap/test/test_hashmap.cpp
@@ -5,5 +5,9 @@
 #include "include/hashmap.h"
 
 TEST(HashMapTest, can_create_hash_map) {
-    ASSERT_NO_THROW(hashmap());
+    ASSERT_NO_THROW(hashmap<>());
+}
+
+TEST(HashMapTest, can_create_hash_with_template_params) {
+    ASSERT_NO_THROW((hashmap<int, double>()));
 }
