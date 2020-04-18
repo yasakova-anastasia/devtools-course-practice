375e0cbf1bb97f87a959ccae1469fd2d97e7322b
Dron228 <savin.duma@yandex.ru>
2020-04-18 19:02:31 +0300

    fix5

--- modules/salary/src/Salary.cpp
+++ modules/salary/src/Salary.cpp
@@ -1,53 +1,42 @@
+// Copyright 2020 SAVIN DMITRIY
 #include "include/Salary.h"
 
-Salary::Salary()
-{
+Salary::Salary() {
 }
 
 
-Salary::~Salary()
-{
+Salary::~Salary() {
 }
 
-float Salary::payment()
-{
-	if (m_hours <= 0)
-	{
-		return 0.0f;
-	}
-	float paymentPerHour = m_salary / m_hours;
-	float payment = (m_hours - m_hours_admin) * paymentPerHour;
-	float paymentOver;
-	if (m_hours_over <= 2) {
-		paymentOver = paymentPerHour * 1.5f * m_hours_over;
-	}
-	else {
-		paymentOver = paymentPerHour * 2.f * (m_hours_over - 2) + paymentPerHour * 1.5f * 2;
-	}
-	/*if (payment < 0)
-	{
-		return 0.0f;
-	}*/
-	payment += paymentOver;
-	return payment;
+float Salary::payment() {
+    if (m_hours <= 0) {
+    return 0.0f;
+    }
+    float paymentPerHour = m_salary / m_hours;
+    float payment = (m_hours - m_hours_admin) * paymentPerHour;
+    float paymentOver;
+    if (m_hours_over <= 2) {
+    paymentOver = paymentPerHour * 1.5f * m_hours_over;
+    }
+    else {
+    paymentOver = paymentPerHour * 2.f * (m_hours_over - 2) + paymentPerHour * 1.5f * 2;
+    }
+    payment += paymentOver;
+    return payment;
 }
 
-void Salary::setSalary(float s)
-{
-	m_salary = s;
+void Salary::setSalary(float s) {
+    m_salary = s;
 }
 
-void Salary::setHours(int h)
-{
-	m_hours = h;
+void Salary::setHours(int h) {
+    m_hours = h;
 }
 
-void Salary::setOverHours(int h)
-{
-	m_hours_over = h;
+void Salary::setOverHours(int h) {
+    m_hours_over = h;
 }
 
-void Salary::setAdminHours(int h)
-{
-	m_hours_admin = h;
+void Salary::setAdminHours(int h) {
+    m_hours_admin = h;
 }
