03bb2b569c04ed117666fcbce7ae0cbe84ed06d8
Boganov Sergei <frixinglife@gmail.com>
2020-04-19 15:37:42 +0300

    Added 'Can_Create_Correct_Object' test

--- modules/numerical-integration/include/numerical_integration.h
+++ modules/numerical-integration/include/numerical_integration.h
@@ -3,6 +3,12 @@
 #ifndef MODULES_NUMERICAL_INTEGRATION_INCLUDE_NUMERICAL_INTEGRATION_H_
 #define MODULES_NUMERICAL_INTEGRATION_INCLUDE_NUMERICAL_INTEGRATION_H_
 
+class NumericalIntegration {
+public:
+   double a, b;
+   double (*f)(double x);   
 
+   NumericalIntegration(double _a, double _b, double (*_f)(double x)) : a(_a), b(_b), f(_f) {};
+};
 
 #endif  // MODULES_NUMERICAL_INTEGRATION_INCLUDE_NUMERICAL_INTEGRATION_H_
--- modules/numerical-integration/test/test_numerical_integration.cpp
+++ modules/numerical-integration/test/test_numerical_integration.cpp
@@ -2,5 +2,38 @@
 
 #include <gtest/gtest.h>
 
+#include <cmath>
+
 #include "include/numerical_integration.h"
 
+class NumericalIntegrationTest : public NumericalIntegration {
+public:
+    double epsilon = 0.001;
+    static double f1(double x)
+    {
+        return x * x;
+    }
+    static double f2(double x)
+    {
+        return 1.0 - x;
+    }
+    static double f3(double x)
+    {
+        return cos(x);
+    }
+};
+
+TEST(NumericalIntegrationTest, Can_Create_Correct_Object) {
+    // Arrange
+    double a = 0.0;
+    double b = 1.0;
+    double x = 3.0;
+
+    // Act
+    NumericalIntegration obj(a, b, NumericalIntegrationTest::f1);
+
+    // Assert
+    EXPECT_EQ(a, obj.a);
+    EXPECT_EQ(b, obj.b);
+    EXPECT_EQ(NumericalIntegrationTest::f1(x), obj.f(x));
+}
