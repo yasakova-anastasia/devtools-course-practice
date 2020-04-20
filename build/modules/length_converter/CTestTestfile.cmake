8c966942b5f296955f143f9e4ddaa8802bb15766
wappah <sokova.nastya9@gmail.com>
2020-04-20 21:36:29 +0300

    fix5

--- build/modules/length_converter/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Stacy/devtools-course-practice/modules/length_converter
-# Build directory: C:/Users/Stacy/devtools-course-practice/build/modules/length_converter
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
