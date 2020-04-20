561fccd3f1836654225c3d1d10c77524a2275755
Alice105973 <kukushkinako13081999@gmail.com>
2020-04-20 14:06:29 +0300

    lint fix#4

--- modules/intersecting-segments/include/segments.h
+++ modules/intersecting-segments/include/segments.h
@@ -4,14 +4,14 @@
 #define MODULES_INTERSECTING_SEGMENTS_INCLUDE_SEGMENTS_H_
 
 class Segment {
+public:
+  Segment(double _x1, double _y1, double _x2, double _y2);
+
+private:
   double x1;
   double y1;
   double x2;
   double y2;
-
-public:
-
-  Segment(double _x1, double _y1, double _x2, double _y2);
 };
 
 #endif  // MODULES_INTERSECTING_SEGMENTS_INCLUDE_SEGMENTS_H_
