c949a8b5fd500663b95309bdb39bc41bd1bdd99d
Alice105973 <kukushkinako13081999@gmail.com>
2020-04-20 17:04:01 +0300

    Intersection function test

--- modules/intersecting-segments/src/segments.cpp
+++ modules/intersecting-segments/src/segments.cpp
@@ -1,5 +1,6 @@
 // Copyright 2020 Kukushkina Ksenia
 
+#include <cmath>
 #include "include/segments.h"
 
 Segment::Segment(double _x1, double _y1, double _x2, double _y2) {
@@ -14,5 +15,12 @@ double Segment::area(double x, double y) {
 }
 
 bool Segment::isIntersect(Segment* A) {
-  return 1;
+  double s1, s2, s3, s4;
+  s1 = A->area(x1, y1);
+  s2 = A->area(x2, y2);
+  s3 = area(A->x1, A->y1);
+  s4 = area(A->x2, A->y2);
+  if (s1 * s2 <= 0 && s3 * s4 <= 0)
+    return true;
+  return false;
 }
