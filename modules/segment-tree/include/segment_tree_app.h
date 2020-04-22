dc8f44eda23f52bcfa9121d413327e2200281f45
Boganov Sergei <frixinglife@gmail.com>
2020-04-22 12:55:17 +0300

    Added new files

---
+++ modules/segment-tree/include/segment_tree_app.h
@@ -0,0 +1,26 @@
+// Copyright 2020 Boganov Sergei
+
+#ifndef MODULES_SEGMENT_TREE_INCLUDE_SEGMENT_TREE_APP_H_
+#define MODULES_SEGMENT_TREE_INCLUDE_SEGMENT_TREE_APP_H_
+
+#include <string>
+
+/*class ComplexCalculator {
+ public:
+    ComplexCalculator();
+    std::string operator()(int argc, const char** argv);
+
+ private:
+    void help(const char* appname, const char* message = "");
+    bool validateNumberOfArguments(int argc, const char** argv);
+    std::string message_;
+    typedef struct {
+        double z1_real;
+        double z1_imaginary;
+        double z2_real;
+        double z2_imaginary;
+        char operation;
+    } Arguments;
+};*/
+
+#endif  // MODULES_SEGMENT_TREE_INCLUDE_SEGMENT_TREE_APP_H_
