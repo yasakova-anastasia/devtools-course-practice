25b27bcb57654409f4b82262e226a7e5df9b8454
381706-2leonipau <leonipau@yandex.ru>
2020-04-23 16:50:58 +0300

    ifndef guard

--- modules/template_heap/include/template_heap.h
+++ modules/template_heap/include/template_heap.h
@@ -1,5 +1,6 @@
 // Copyright 2020 Pauzin Leonid
-
+#ifndef MODULES_TEMPLATE_HEAP_INCLUDE_TEMPLATE_HEAP_H_
+#define MODULES_TEMPLATE_HEAP_INCLUDE_TEMPLATE_HEAP_H_
 #include <vector>
 
 template <typename T>
@@ -7,7 +8,10 @@ class THeap {
 public:
   THeap() = default;
 
+
 private:
   std::vector<T> vec;
 
 };
+
+#endif MODULES_TEMPLATE_HEAP_INCLUDE_TEMPLATE_HEAP_H_
