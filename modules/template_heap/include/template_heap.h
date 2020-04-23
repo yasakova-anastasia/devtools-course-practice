0b394f5998c31bc3df79eef9d9e19c30354ee231
381706-2leonipau <leonipau@yandex.ru>
2020-04-23 17:25:59 +0300

    Some updates

--- modules/template_heap/include/template_heap.h
+++ modules/template_heap/include/template_heap.h
@@ -5,13 +5,42 @@
 #include <vector>
 
 template <typename T>
- class THeap {
-public:
+class THeap {
+ public:
   THeap() = default;
+  void Push(T value);
 
  private:
   std::vector<T> vec;
-
+  void Emersion();
+  int GetParent(int child);
 };
+
+template<typename T>
+void THeap<T>::Push(T value) {
+  vec.push_back(value);
+  Emersion();
+}
+
+template<typename T>
+void THeap<T>::Emersion() {
+  int child = vec.size() - 1;
+  int parent = GetParent(child);
+
+  while (vec[child] > vec[parent] && child >= 0 && parent >= 0) {
+    std::swap(child, parent);
+    child = parent;
+    parent = GetParent(child);
+  }
+}
+
+template <typename T>
+int THeap<T>::GetParent(int child) {
+  if (child > 0 && child % 2 == 0)
+    return child / 2 - 1;
+  else
+    return child / 2;
+}
+
 
 #endif MODULES_TEMPLATE_HEAP_INCLUDE_TEMPLATE_HEAP_H_
--- modules/template_heap/test/test_template_heap.cpp
+++ modules/template_heap/test/test_template_heap.cpp
@@ -3,14 +3,19 @@
 #include <gtest/gtest.h>
 #include "include/template_heap.h"
 
-TEST(Theap, Can_Create_int_heap) {
+TEST(THeap, Can_Create_int_heap) {
   ASSERT_NO_THROW(THeap<int>());
 }
 
-TEST(Theap, Can_Create_double_heap) {
+TEST(THeap, Can_Create_double_heap) {
   ASSERT_NO_THROW(THeap<double>());
 }
 
-TEST(Theap, Can_Create_char_heap) {
+TEST(THeap, Can_Create_char_heap) {
   ASSERT_NO_THROW(THeap<char>());
+}
+
+TEST(THeap, Can_insert_value) {
+  THeap<int> a;
+  ASSERT_NO_THROW(a.Push(3));
 }
