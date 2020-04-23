6388640e1af42d4317335bc42a2713bf8a2a0a3a
381706-2leonipau <leonipau@yandex.ru>
2020-04-23 16:57:20 +0300

    codestyle fix

--- modules/template_heap/include/template_heap.h
+++ modules/template_heap/include/template_heap.h
@@ -1,15 +1,15 @@
 // Copyright 2020 Pauzin Leonid
 #ifndef MODULES_TEMPLATE_HEAP_INCLUDE_TEMPLATE_HEAP_H_
 #define MODULES_TEMPLATE_HEAP_INCLUDE_TEMPLATE_HEAP_H_
+
 #include <vector>
 
 template <typename T>
-class THeap {
+ class THeap {
 public:
   THeap() = default;
 
-
-private:
+ private:
   std::vector<T> vec;
 
 };
