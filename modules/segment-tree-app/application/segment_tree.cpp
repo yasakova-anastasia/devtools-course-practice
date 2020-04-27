894ba904c0dfe788efa813694cfeb461964d4d3d
Boganov Sergei <frixinglife@gmail.com>
2020-04-28 01:28:03 +0300

    Renamed folder

---
+++ modules/segment-tree-app/application/segment_tree.cpp
@@ -0,0 +1,14 @@
+// Copyright 2020 Boganov Sergei
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+#include "include/segment_tree_app.h"
+
+int main(int argc, const char** argv) {
+    SegmentTreeApp app;
+    std::string output = app(argc, argv);
+    printf("%s\n", output.c_str());
+    return 0;
+}
