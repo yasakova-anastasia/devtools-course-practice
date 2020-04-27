70b2c9b1a0e95b559aaec5fa451531a2e8a942e1
IconLivin <anton.parole@mail.ru>
2020-04-27 16:47:47 +0300

    Resolved some issues

--- modules/prime_numbers/include/TPrime_Nums.h
+++ modules/prime_numbers/include/TPrime_Nums.h
@@ -12,10 +12,10 @@ class TPrime_Nums {
  public:
     TPrime_Nums(int left, int right);
     std::pair<int, int> GetInterval();
-    std::vector<int> Get_Prime_Nums();
+    std::vector<unsigned int> Get_Prime_Nums();
     void SetInterval(std::pair<int, int> interval);
  private:
-    int left_edge, right_edge;
+    unsigned int left_edge, right_edge;
 };
 
 #endif  // MODULES_PRIME_NUMBERS_INCLUDE_TPRIME_NUMS_H_
