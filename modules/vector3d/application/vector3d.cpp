da09ce44fb6d503015fa9598f010a2246c676dc6
Alexey <kret906alex@gmail.com>
2020-04-29 15:52:05 +0300

    Add fictitious check test and app files

---
+++ modules/vector3d/application/vector3d.cpp
@@ -0,0 +1,13 @@
+// Copyright 2020 Baldin Alexey
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+#include "include/vector3D_calculator.h"
+
+int main(int argc, const char** argv) {
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
---
+++ modules/vector3d/test/test_3d_vector_calculator.cpp
@@ -0,0 +1,11 @@
+// Copyright 2020 Baldin Alexey
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include "include/vector3D.h"
+
+TEST(CheckTests, CheckTest) {
+    EXPECT_TRUE(true);
+}
