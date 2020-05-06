4edecf3e7baace8694d42f566189361342e05fa9
SinM9 <marusya19.09.99@gmail.com>
2020-05-06 19:41:48 +0300

    fix

--- modules/queue/test/test_queue.cpp
+++ modules/queue/test/test_queue.cpp
@@ -32,24 +32,20 @@ TEST(Queue, Can_Put_Element) {
 TEST(Queue, Cant_Put_Element_In_Full) {
     // Arrange
     Queue q(3);
-
-    // Act
     q.Put(1);
     q.Put(2);
     q.Put(3);
 
-    // Assert
+    // Act & Assert
     EXPECT_ANY_THROW(q.Put(4));
 }
 
 TEST(Queue, Can_Get_Element) {
     // Arrange
     Queue q(5);
-
-    // Act
     q.Put(1);
 
-    // Assert
+    // Act & Assert
     EXPECT_NO_THROW(q.Get());
 }
 
@@ -64,15 +60,13 @@ TEST(Queue, Cant_Get_Element_In_Empty) {
 TEST(Queue, Get_Element_Is_Correct) {
     // Arrange
     Queue q(3);
-
-    // Act
     q.Put(1);
     q.Put(2);
     q.Put(3);
     q.Get();
     q.Put(4);
 
-    // Assert
+    // Act & Assert
     EXPECT_EQ(2, q.Get());
 }
 
@@ -80,26 +74,22 @@ TEST(Queue, Get_Element_Is_Correct) {
 TEST(Queue, Top_Element_Is_Different_After_Get) {
     // Arrange
     Queue q(5);
-
-    // Act
     q.Put(1);
     q.Put(2);
     q.Get();
 
-    // Assert
+    // Act & Assert
     EXPECT_EQ(2, q.TopElem());
 }
 
 TEST(Queue, Top_Elem_Function_Doesnt_Delete_Top_Element) {
     // Arrange
     Queue q(3);
-
-    // Act
     q.Put(2);
     q.Put(3);
     q.TopElem();
 
-    // Assert
+    // Act & Assert
     EXPECT_EQ(2, q.TopElem());
 }
 
@@ -114,11 +104,9 @@ TEST(Queue, Cant_Call_Top_Elem_In_Empty_Queue) {
 TEST(Queue, Get_Count_Is_Correct) {
     // Arrange
     Queue q(3);
-
-    // Act
     q.Put(1);
     q.Put(2);
 
-    // Assert
+    // Act & Assert
     EXPECT_EQ(2, q.GetCount());
 }
