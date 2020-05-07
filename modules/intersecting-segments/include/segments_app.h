0544c38531ef1c7582e36b6b538d3a021e2e499d
IrinaMaximova-381706 <ira.maximova2017@yandex.ru>
2020-05-07 22:27:35 +0300

    Help function added

--- modules/intersecting-segments/include/segments_app.h
+++ modules/intersecting-segments/include/segments_app.h
@@ -3,7 +3,16 @@
 #ifndef MODULES_INTERSECTING_SEGMENTS_INCLUDE_SEGMENTS_APP_H_
 #define MODULES_INTERSECTING_SEGMENTS_INCLUDE_SEGMENTS_APP_H_
 
+#include <string>
+
 class Segment_app {
+ public:
+  Segment_app();
+
+ private:
+  void help(const char* appname, const char* message = "");
+  std::string message_;
+  bool validateNumberOfArguments(int argc, const char** argv);
 };
 
 #endif  // MODULES_INTERSECTING_SEGMENTS_INCLUDE_SEGMENTS_APP_H_
--- modules/intersecting-segments/src/segments_app.cpp
+++ modules/intersecting-segments/src/segments_app.cpp
@@ -1,3 +1,31 @@
 // Copyright 2020 Maximova Irina
 
+#include <string>
+
 #include "include/segments_app.h"
+
+Segment_app::Segment_app() : message_("") {}
+
+void Segment_app::help(const char* appname, const char* message) {
+  message_ =
+    std::string(message) +
+    "This is a intersecting segments application.\n\n" +
+    "Please provide arguments in the following format:\n\n" +
+
+    "  $ " + appname + " <x11> <y11> " +
+    "<x12> <y12> \n\n" + " <x21> <y21> " +
+    "<x22> <y22> \n\n" +
+
+    "Where all arguments are double-precision numbers\n";
+}
+
+bool Segment_app::validateNumberOfArguments(int argc, const char** argv) {
+  if (argc == 1) {
+    help(argv[0]);
+    return false;
+  } else if (argc != 9) {
+    help(argv[0], "ERROR: Should be 8 arguments.\n\n");
+    return false;
+  }
+  return true;
+}
