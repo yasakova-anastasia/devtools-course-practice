f481b6909b26336dfad70edfdae3a285b73905bd
wappah <sokova.nastya9@gmail.com>
2020-05-12 19:20:33 +0300

    changes 2 fix

--- modules/length_converter/include/length_converter.h
+++ modules/length_converter/include/length_converter.h
@@ -8,7 +8,8 @@ class LengthConverter {
     bool side;
  public:
     LengthConverter() : curr(0), side(false) {}
-    LengthConverter(double currency, bool amount) : curr(currency), side(amount) {}
+    LengthConverter(double currency, bool amount) 
+        : curr(currency), side(amount) {}
     double m_kmeter(const double& curr, bool side);
     double m_smeter(const double& curr, bool side);
     double m_mile(const double& curr, bool side);
